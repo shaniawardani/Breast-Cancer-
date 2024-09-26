4dc3145876bd258beec9fda09dea856a0bd5b9c1
shaniawardani <162453637+shaniawardani@users.noreply.github.com>
2024-09-27 06:29:15 +0700

    update Login.py

--- Login.py
+++ Login.py
@@ -9,48 +9,47 @@ def set_page_config():
         initial_sidebar_state="expanded",
     )
 
-# Fungsi untuk memeriksa autentikasi pengguna
 def authenticate(username, password):
-    # Database pengguna sederhana (contoh)
+    
     valid_users = {
         "shania": "12345678",
         "test": "test123",
     }
     
-    # Cek apakah username dan password cocok
+   
     if username in valid_users and valid_users[username] == password:
         return True
     else:
         return False
 
-# Fungsi untuk form login
+
 def login_form():
     st.title("Login")
 
-    # Input untuk username dan password
+    
     username = st.text_input("username")
     password = st.text_input("Password", type="password")
 
-    # Tombol login
+    
     if st.button("Login"):
         if authenticate(username, password):
-            st.session_state['logged_in'] = True  # Simpan status login
-            st.session_state['username'] = username  # Simpan username
+            st.session_state['logged_in'] = True  
+            st.session_state['username'] = username 
             st.success("Login Successful!")
         else:
             st.error("Username or Password false!")
 
-# Fungsi utama untuk aplikasi
+
 def main():
-    # Cek apakah pengguna sudah login
+    
     if 'logged_in' not in st.session_state:
         st.session_state['logged_in'] = False
 
-    # Jika sudah login, redirect ke halaman Home.py
+    
     if st.session_state['logged_in']:
-        st.write ("[Login Successful](./pages/1-Home.py)")  # Pengaturan untuk alihkan ke halaman Home
+        st.write ("[Login Successful](./pages/1-Home.py)") 
     else:
-        login_form()  # Tampilkan form login jika belum login
+        login_form()
 
 if __name__ == '__main__':
     main()
